1a12129e5519d13bea981ace41c6bad0b4a6ea53
Louis JEAN <spideai@outlook.fr>
2022-12-16 14:58:20 +0100

    fix

--- main.js
+++ main.js
@@ -1,46 +1,55 @@
+// document.querySelector('#check-all-right-answers').addEventListener('click', (event) => {
+//   chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+//     chrome.scripting.executeScript(
+//       {
+//         target: {tabId: tabs[0].id},
+//         files: ['checkAllRightAnswers.js'],
+//       },
+//   );
+//
+//   });
+//
+// });
 
-document.querySelector('#check-all-right-answers').addEventListener('click', (event) => {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
-    chrome.scripting.executeScript(
-      {
-        target: {tabId: tabs[0].id},
-        files: ['checkAllRightAnswers.js'],
-      },
-  );
+// document.querySelector('#validate').addEventListener('click', (event) => {
+//   chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+//     chrome.scripting.executeScript(
+//       {
+//         target: {tabId: tabs[0].id},
+//         files: ['validate.js'],
+//       },
+//   );
+//   });
+// });
 
-  });
-
-});
-
-document.querySelector('#validate').addEventListener('click', (event) => {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
-    chrome.scripting.executeScript(
-      {
-        target: {tabId: tabs[0].id},
-        files: ['validate.js'],
-      },
-  );
-  });
-});
-
+// document.querySelector('#brute-type-2').addEventListener('click', (event) => {
+//   chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+//     chrome.scripting.executeScript(
+//       {
+//         target: {tabId: tabs[0].id},
+//         files: ['brute-type-2.js'],
+//       },
+//   );
+//   });
+// });
 
 document.querySelector('#brute').addEventListener('click', (event) => {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
-    chrome.scripting.executeScript(
-      {
-        target: {tabId: tabs[0].id},
-        files: ['brute.js'],
-      },
-  );
-  });
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+        chrome.scripting.executeScript(
+            {
+                target: {tabId: tabs[0].id},
+                files: ['brute.js'],
+            },
+        );
+    });
 });
 
-document.querySelector('#brute-type-2').addEventListener('click', (event) => {
+document.querySelector('#global-exam').addEventListener('click', (event) => {
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
     chrome.scripting.executeScript(
       {
         target: {tabId: tabs[0].id},
-        files: ['brute-type-2.js'],
+        files: ['openGlobalExam.js'],
       },
   );
   });
---
+++ openGlobalExam.js
@@ -0,0 +1,1 @@
+window.open("https://auth.global-exam.com/sso/cas/ynov/10010", "_blank");
